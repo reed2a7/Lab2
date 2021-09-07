0120772ff2562c5630fba948122f334ecc9054c7
reed2a7 <89818492+reed2a7@users.noreply.github.com>
2021-09-07 22:18:45 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("M&M Color Counts");
+
+    int total = 55*11;
+    double blue = total * .24;
+    double brown = total * .13;
+    double green = total * .16;
+    double orange = total *.20;
+    double red = total * .13;
+    double yellow = total * .14;
+    System.out.println("blue: " + blue);
+    System.out.println("brown: " + brown);
+    System.out.println("green: " + green);
+    System.out.println("orange: " + orange);
+    System.out.println("red: " + red);
+    System.out.println("yellow: " + yellow);
+    
+    double sum = blue + brown + green + orange + red + yellow;
+    System.out.println("sum: " + sum);
+    if (blue < brown && red > orange)
+       System.out.println("Blue under Brown and Red over Orange");
+    if (brown >= green)
+       System.out.println("Brown is greater than or equal to Green");
+    if (sum == total)
+      System.out.println("Numbers match");
+  }
+}
